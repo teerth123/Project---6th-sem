a66bf613d74dd880d67583c55c6d58745d973210
DexT_17 Teerth <teerth.kulkarni@walchandsangli.ac.in>
2025-05-03 21:21:07 +0530

    new route added

--- server/index.js
+++ server/index.js
@@ -75,8 +75,14 @@ app.get('/', (req, res) => {
   });
 })
 
+app.get('/teerth', (req, res) => {
+  res.json({
+    msg:"teerth"
+  })
+})
+
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
