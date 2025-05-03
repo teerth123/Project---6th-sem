87433e85aafdaf275441940e970eb8225d2d915b
DexT_17 Teerth <teerth.kulkarni@walchandsangli.ac.in>
2025-05-03 21:37:25 +0530

    changed to 3001

--- server/index.js
+++ server/index.js
@@ -82,7 +82,7 @@ app.get('/teerth', (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
